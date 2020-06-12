ced525889a5300673d7daf1081299b356c9b8a15
Dominik Duda <dominik.duda6@gmail.com>
2020-06-12 23:10:44 +0200

    made it prettier, added json data + imgs

--- script/main.js
+++ script/main.js
@@ -1,58 +1,70 @@
 // holt die externe Json file und wandelt sie in ein object um
 let readJSON = function (file) {
-    let json = {}
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': file,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-    });
-    return json;
-};
+  let json = {}
+  $.ajax({
+    async: false,
+    global: false,
+    url: file,
+    dataType: 'json',
+    success: function (data) {
+      json = data
+    }
+  })
+  return json
+}
 
-let film = readJSON("script/film.json");
-console.table(film);
+let film = readJSON('script/film.json')
+console.table(film)
 
-console.log(typeof(film));
+console.log(typeof film)
 
+$(document).ready(function () {
+  // Wenn alles fertig geladen ist
+  filmInsert($('.container'))
 
-$(document).ready(function() {  // Wenn alles fertig geladen ist 
-    filmInsert($('.container'));
-
-
-
-// holt die value vom click counter und fügt 1 dazu
-$(document).on("click", ".textbox button", function() {
-    let likes = parseInt($(this).closest(".textbox").find("span").text());
+  // holt die value vom click counter und fügt 1 dazu
+  $(document).on('click', '.textbox button', function () {
+    let likes = parseInt(
+      $(this)
+        .closest('.textbox')
+        .find('span')
+        .text()
+    )
     console.log(likes)
-    $(this).closest(".textbox").find("span").text(likes + 1);
-  });
- 
+    $(this)
+      .closest('.textbox')
+      .find('span')
+      .text(likes + 1)
+  })
 
- // speichert alle divs in der variable divs als arr und danach wirds sortiert anhand der like values mit der sort funktion
-  $("#sort").on("click", function() {
-    let divs = $(".boxwrapper")
+  // speichert alle divs in der variable divs als arr und danach wirds sortiert anhand der like values mit der sort funktion
+  $('#sort').on('click', function () {
+    let divs = $('.boxwrapper')
 
-    let sorted = divs.sort(function(a, b) {
-        return parseInt($(a).find("span").text()) < parseInt($(b).find("span").text()) ? 1 : -1;
-            
-    });
+    let sorted = divs.sort(function (a, b) {
+      return parseInt(
+        $(a)
+          .find('span')
+          .text()
+      ) <
+        parseInt(
+          $(b)
+            .find('span')
+            .text()
+        )
+        ? 1
+        : -1
+    })
     Swal.fire('SORTING DONE -- MY MASTER')
-    $(".container").append(sorted);
-      
-  });
- 
+    $('.container').append(sorted)
+  })
 
-    function filmInsert(insert) {
-        $.each(film, function(i, data) { //.each statt loop
-           
-          
-          let box = 
-          
-          `<div class="boxwrapper" id="${i++}" data=[0]>
+  //JSON Daten einfügen in die webseite
+  function filmInsert (insert) {
+    $.each(film, function (i, data) {
+      //.each statt loop
+
+      let box = `<div class="boxwrapper" id="${i++}" data=[0]>
           <div class="imgbox">
           <img src="${data.img}" alt="${data.titel}">
         </div>
@@ -64,23 +76,8 @@ $(document).on("click", ".textbox button", function() {
               <span id="output${data.id}" class="counter${data.id}">0</span>
             </p>
           </div>
-        </div>`;
-          insert.append(box);
-        
-        });
-      }
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </div>`
+      insert.append(box)
+    })
+  }
+})
